49b710986ade145c06310848cca6fb4e4fd113d9
Mike Monji <mikemonji@Mikes-Laptop.local>
2025-03-30 15:38:23 +0200

    feat(store): setup redux toolkit, redux persist, and root reducer

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./rootReducer";
+import { persistStore } from "redux-persist";
+import { persistReducer } from "./persist";
+
+export const store = configureStore({
+  reducer: persistReducer(rootReducer),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
